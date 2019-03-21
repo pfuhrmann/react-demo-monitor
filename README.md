97f075d8febaa93bac58b963e4d27203945b49be
Patrik Fuhrmann <fuhrmann.patrik@gmail.com>
2019-03-21 22:13:41 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Monitor
+Your simple health tracker
+
+### Requirements
+- `yarn` (or `npm`)
+
+### Start dev server
+```
+yarn start
+```
+
+### Build for production
+```
+yarn build
+```
+
+### Run tests
+```
+yarn tests
+```
---
+++ src/components/app/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-box">
+            Edit <code>src/App.js</code> and save to reload.
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React.js
+            </a>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
